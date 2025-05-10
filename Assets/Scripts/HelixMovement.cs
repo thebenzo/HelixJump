5cc8963ea5a08c39eefbdd10e30ed3ff8a5112e8
Aman <amanvrma5799@gmail.com>
2025-05-10 19:32:39 +0530

    Implement helix rotation controls

---
+++ Assets/Scripts/HelixMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HelixMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 5.0f;
+
+    private Vector2 _mouseDelta;
+    private bool _leftMouseButtonDown;
+
+    // Input action messages
+    public void OnRotate(InputValue input)
+    {
+        _mouseDelta = input.Get<Vector2>();
+    }
+
+    public void OnRotateButton(InputValue input)
+    {
+        _leftMouseButtonDown = input.Get<float>() == 1;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Apply Y rotation to the helix only if player is holding left mouse button
+        if (_leftMouseButtonDown)
+        {
+            float rotationY = _mouseDelta.x * _rotationSpeed * Time.deltaTime;
+
+            transform.Rotate(Vector3.up, -rotationY, Space.World);
+        }
+    }
+}
